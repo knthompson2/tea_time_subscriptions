ad603ea90929e5f30283abfd78ade2388fcf61f6
Kelsey Thompson <kelsthompson2@gmail.com>
2022-01-07 05:38:01 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customers
+        get "customers/:id/subscriptions", to: "customers/subscriptions#index"
+        post "customers/:id/subscriptions", to: "customers/subscriptions#create"
+        patch "customers/:id/subscriptions/:subscription_id", to: "customers/subscriptions#update"
+    end
+  end
 end
